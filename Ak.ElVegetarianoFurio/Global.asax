317a3ab36247d940c5a82805afdda90f6ade4a23
Andre Kraemer <andre@andrekraemer.de>
2017-08-18 20:47:15 +0200

    Projektdateien hinzufügen.

---
+++ Ak.ElVegetarianoFurio/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ak.ElVegetarianoFurio.MvcApplication" Language="C#" %>
---
+++ Ak.ElVegetarianoFurio/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ak.ElVegetarianoFurio.Startup))]
+namespace Ak.ElVegetarianoFurio
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
